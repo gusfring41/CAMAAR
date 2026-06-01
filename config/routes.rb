6e9b38cd71e2c3657d1ced5c2439b6ecf8aea3ef
neatzzy <elviscorreamirandajr@gmail.com>
2026-06-01 17:55:07 -0300

    :wrench: fix: consertado erro de sintaxe das rotas

--- config/routes.rb
+++ config/routes.rb
@@ -19,8 +19,8 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  get 'admin', to 'admin#index', as :admin
-  post 'admin/importar_sigaa' to 'admin#importar_sigaa', as :importar_sigaa
+  get "admin", to: "admin#index", as: :admin
+  post "admin/importar_sigaa", to: "admin#importar_sigaa", as: :importar_sigaa
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
